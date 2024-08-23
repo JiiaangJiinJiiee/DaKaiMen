2bac3695bf8168d9e2fe2e4ac8d1c5ee0ec51353
JiiaangJiinJiiee <JiiaangJiinJiiee@outlook.com>
2024-08-23 13:53:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ root.rowconfigure(0, weight=1)
 
 ttk.Label(mainframe, text="====施工中====").grid(column=1, row=0)
 
-ttk.Button(mainframe, width=5, text="辞职！", command=exit).grid(column=1, row=2)
+ttk.Button(mainframe, width=5, text="辞职", command=exit).grid(column=1, row=2)
 
 root.mainloop()
